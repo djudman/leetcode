7bd3f5a2c9b82329de059af82f2aabcdfe00d0c0
Dmitrii Dorofeev <dorofeev@game-insight.com>
2020-03-01 16:45:20 +0300

    Separated methods __str__ and __repr__ for TreeNode.  Also, a bug fixed in make_binary_tree() function.

--- leetcode/utils/tree.py
+++ leetcode/utils/tree.py
@@ -1,3 +1,4 @@
+import json
 from typing import List, Optional
 
 
@@ -8,7 +9,27 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-    def __repr__(self, intend: str = '') -> str:
+    def __str__(self, intend: str = '') -> str:
+        """
+        >>> tree = TreeNode(1)
+        >>> tree.left = TreeNode(2)
+        >>> tree.right = TreeNode(3)
+        >>> tree.left.left = TreeNode(4)
+        >>> tree.left.right = TreeNode(5)
+        >>> str(tree)
+        '1\\n\\t2\\n\\t\\t4\\n\\t\\t5\\n\\n\\t3\\n'
+        """
+        left_value = intend
+        right_value = intend
+        if self.left:
+            left_value = self.left.__str__(intend=f'{intend}\t')
+        if self.right:
+            right_value = self.right.__str__(intend=f'{intend}\t')
+        if self.left or self.right:
+            return f'{intend}{self.val}\n{left_value}\n{right_value}\n'
+        return f'{intend}{self.val}'
+
+    def __repr__(self) -> str:
         """
         >>> tree = TreeNode(1)
         >>> tree.left = TreeNode(2)
@@ -16,19 +37,20 @@ class TreeNode(object):
         >>> tree.left.left = TreeNode(4)
         >>> tree.left.right = TreeNode(5)
         >>> repr(tree)
-        '1\\n\\t2\\n\\t\\t4\\n\\t\\t5\\n\\n\\t3\\n'
-        >>> str(tree)
-        '1\\n\\t2\\n\\t\\t4\\n\\t\\t5\\n\\n\\t3\\n'
+        '[1, 2, 3, 4, 5]'
         """
-        left_value = intend
-        right_value = intend
-        if self.left:
-            left_value = self.left.__repr__(intend=f'{intend}\t')
-        if self.right:
-            right_value = self.right.__repr__(intend=f'{intend}\t')
+        out = [self.val]
+        out.extend(self.get_values())
+        return json.dumps(out)
+
+    def get_values(self) -> List:
+        values = []
         if self.left or self.right:
-            return f'{intend}{self.val}\n{left_value}\n{right_value}\n'
-        return f'{intend}{self.val}'
+            values.append(self.left and self.left.val or None)
+            values.append(self.right and self.right.val or None)
+            values.extend(self.left.get_values())
+            values.extend(self.right.get_values())
+        return values
 
 
 def make_binary_tree(values: List) -> Optional[TreeNode]:
@@ -43,15 +65,27 @@ def make_binary_tree(values: List) -> Optional[TreeNode]:
     #   etc.
 
     >>> make_binary_tree([0])
-    0
-    >>> make_binary_tree([0, 1, 2]) # doctest: +NORMALIZE_WHITESPACE
-    0\n\t1\n\t2\n\n
-    >>> make_binary_tree([0, None, 2]) # doctest: +NORMALIZE_WHITESPACE
-    0\n\tNone\n\t2\n\n
-    >>> make_binary_tree([0, 1, 2, 3, 4, 5, 6]) # doctest: +NORMALIZE_WHITESPACE
-    0\n\t1\n\t\t3\n\t\t4\n\t2\n\t\t5\n\t\t6\n\n
-    >>> make_binary_tree([0, 1, 2, 3, 4, None, 6]) # doctest: +NORMALIZE_WHITESPACE
-    0\n\t1\n\t\t3\n\t\t4\n\t2\n\t\tNone\n\t\t6\n\n
+    [0]
+    >>> tree = make_binary_tree([0, 1, 2])
+    >>> str(tree)  # doctest: +NORMALIZE_WHITESPACE
+    '0\\n\\t1\\n\\t2\\n'
+    >>> tree
+    [0, 1, 2]
+    >>> tree = make_binary_tree([0, None, 2])
+    >>> str(tree)  # doctest: +NORMALIZE_WHITESPACE
+    '0\\n\\tNone\\n\\t2\\n'
+    >>> tree
+    [0, null, 2]
+    >>> tree = make_binary_tree([0, 1, 2, 3, 4, 5, 6])
+    >>> str(tree) # doctest: +NORMALIZE_WHITESPACE
+    '0\\n\\t1\\n\\t\\t3\\n\\t\\t4\\n\\n\\t2\\n\\t\\t5\\n\\t\\t6\\n\\n'
+    >>> tree
+    [0, 1, 2, 3, 4, 5, 6]
+    >>> tree = make_binary_tree([0, 1, 2, 3, 4, None, 6])
+    >>> str(tree) # doctest: +NORMALIZE_WHITESPACE
+    '0\\n\\t1\\n\\t\\t3\\n\\t\\t4\\n\\n\\t2\\n\\t\\tNone\\n\\t\\t6\\n\\n'
+    >>> tree
+    [0, 1, 2, 3, 4, null, 6]
     """
     if not values:
         return
@@ -63,11 +97,13 @@ def make_binary_tree(values: List) -> Optional[TreeNode]:
         right_index = 2 * i + 2
         if left_index < len(values):
             node = TreeNode(values[left_index])
-            nodes[left_index] = node
+            if nodes[left_index] is None:
+                nodes[left_index] = node
             nodes[i].left = node
         if right_index < len(values):
             node = TreeNode(values[right_index])
-            nodes[right_index] = node
+            if nodes[right_index] is None:
+                nodes[right_index] = node
             nodes[i].right = node
     return nodes[0]
 
