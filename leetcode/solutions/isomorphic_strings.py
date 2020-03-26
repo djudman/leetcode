251672c9f74996bf05d5a2da602d200280f0cd97
Dmitrii Dorofeev <dorofeev@game-insight.com>
2020-03-26 11:51:03 +0300

    205. Isomorphic Strings (https://leetcode.com/problems/isomorphic-strings/)

---
+++ leetcode/solutions/isomorphic_strings.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+
+from leetcode.utils.list import ListNode, make_linked_list
+from leetcode.utils.tree import TreeNode, make_binary_tree
+
+
+# https://leetcode.com/problems/isomorphic-strings/
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        """
+        >>> solution = Solution()
+
+        >>> solution.isIsomorphic('egg', 'add')
+        True
+        >>> solution.isIsomorphic('foo', 'bar')
+        False
+        >>> solution.isIsomorphic('paper', 'title')
+        True
+        >>> solution.isIsomorphic('aa', 'aa')
+        True
+        >>> solution.isIsomorphic('aa', 'bb')
+        True
+        >>> solution.isIsomorphic('abc', 'cab')
+        True
+        >>> solution.isIsomorphic('abc', 'cbb')
+        False
+        >>> solution.isIsomorphic('ab', 'aa')
+        False
+        """
+        if len(s) != len(t):
+            return False
+        alphabet = {}
+        used = {}
+        for i in range(len(s)):
+            char = alphabet.get(s[i])
+            if char:
+                if char != t[i]:
+                    return False
+            elif t[i] in used:
+                return False
+            alphabet[s[i]] = t[i]
+            used[t[i]] = True
+        return True
